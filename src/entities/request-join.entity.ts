8e66ca9479c4c951f8f4291cc689d0465ee6c68d
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-03-07 11:07:05 +0900

    req-join entity(?)

--- src/entities/request-join.entity.ts
+++ src/entities/request-join.entity.ts
@@ -1,4 +1,12 @@
-import { Entity } from 'typeorm';
+import { Entity, ManyToOne } from 'typeorm';
+import { club } from './club.entity';
+import { User } from './user.entity';
 
 @Entity()
-export class join {}
+export class join {
+  @ManyToOne(() => User, (User) => User.id)
+  userId: number;
+
+  @ManyToOne(() => club, (club) => club.id)
+  clubId: number;
+}
