8f62ddecbebd44939aeeb0db261f572e953c8fdd
시훈 <81404026+sihun96@users.noreply.github.com>
2022-03-07 09:32:44 +0900

    이미지 엔티티

--- src/entities/image.entity.ts
+++ src/entities/image.entity.ts
@@ -1,4 +1,14 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { club } from './club.entity';
 
 @Entity()
-export class image {}
+export class image {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => club, (club) => club.id)
+  club_id: club;
+
+  @Column()
+  uri: string;
+}
