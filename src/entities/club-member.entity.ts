3c4964abd1a78adc09e9731649f398a77e4579bf
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-03-07 10:20:01 +0900

    notification entity

--- src/entities/club-member.entity.ts
+++ src/entities/club-member.entity.ts
@@ -1,4 +1,9 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class clubmember {}
+export class clubmember {
+  @PrimaryGeneratedColumn()
+  userId: number;
+  @Column()
+  clubId: number;
+}
--- src/entities/notification.entity.ts
+++ src/entities/notification.entity.ts
@@ -1,4 +1,14 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { club } from './club.entity';
 
 @Entity()
-export class notifiacion {}
+export class notifiacion {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @ManyToOne(() => club, (club) => club.id)
+  clubId: number;
+  @Column()
+  title: string;
+  @Column()
+  content: string;
+}
