bd6dcf44689c3bcd3a7b5bf6c89bce1f4fd12abd
시훈 <81404026+sihun96@users.noreply.github.com>
2022-03-07 09:33:09 +0900

    club entity

--- src/entities/club.entity.ts
+++ src/entities/club.entity.ts
@@ -1,4 +1,31 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class club {}
+export class club {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  type: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  teacher: string;
+
+  @Column()
+  head: string;
+
+  @Column()
+  discord: string;
+
+  @Column('simple-array')
+  clubMember: string[];
+
+  @Column('simple-array')
+  clubPicture: string[];
+}
